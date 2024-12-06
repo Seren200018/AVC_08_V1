88ee35ef60e6d084996e6284bce873b45b99d4bb
Christian Pommer <Christian.Pommer@gmx.de>
2024-12-06 18:15:34 +0100

    Liveupdate

--- src/SpringMassDamper.mjs
+++ src/SpringMassDamper.mjs
@@ -1,5 +1,4 @@
 import {create, all, string} from 'mathjs';
-
 import * as JXG from "jsxgraph";
 
 const config = {
@@ -12,7 +11,11 @@ const config = {
   randomSeed: null
 };
 const math = create(all, config);
-
+function linspace(start, stop, num, endpoint = true) {
+  const div = endpoint ? (num - 1) : num;
+  const step = (stop - start) / div;
+  return Array.from({length: num}, (_, i) => start + step * i);
+}
 export class mass_spring_damper {
   m = 1;
   c = 0.1;
@@ -80,40 +83,58 @@ document.getScroll = function() {
 
 function livemassspringdamper()
 {
-  let board = JXG.JSXGraph.initBoard('app',{pan: {enabled:false},showNavigation:false, browserPan: {enabled:false},axis: true, grid: false});
-  let y= math.zeros(100).toArray();
-  let t = math.range(0,10,0.3,true).toArray();
+
+  let t_max = 100;
+  let Pointnumber = 200;
+
+  let t = [];
+  let y = [];
+  let last_t = 0
+  let board = JXG.JSXGraph.initBoard('app',{boundingbox: [0, 1, 40, -1],pan: {enabled:false},showNavigation:false, browserPan: {enabled:false},axis: true, grid: false});
+  //let y= math.zeros(Pointnumber).toArray();
+ // let t = math.range(0,10,0.3,true).toArray();
   let scrollold = document.getScroll();
 
-  let MSD = new mass_spring_damper(1, 0.3, 1);
+  let MSD = new mass_spring_damper(1, 0.3, 5);
   let g3 = board.create('curve', [t, y], {strokeColor: 'red', strokeWidth: '2px'});
   let x_0 = [1,0];
+  let Lastcycle = Date.now();
   function Calc_and_draw() {
     let scrollnew = document.getScroll();
-    x_0 = [scrollnew[1]/10-scrollold[1]/10+x_0[0],x_0[1]]
+    x_0 = [scrollnew[1]/100-scrollold[1]/100+x_0[0],x_0[1]]
     scrollold=scrollnew;
     //let Result =math.solveODE(MSD.mathspringdamper_sim, [0, 0.1], x_0)
 
-    // let t = MSD.gettimeseries(0, 100, 10 / 100);
-    let out= MSD.solvemsd(math.matrix([0,0.2]),x_0);
+    let Cycletime = (Date.now()-Lastcycle)/1000
+    Lastcycle = Date.now();
 
-    y.unshift(out[1][0]);
+    let out= MSD.solvemsd(math.matrix([0,Cycletime]),x_0);
+
+    last_t += Cycletime;
+    t.push(last_t);
+    y.push(out[1][0]);
     x_0 = out[1];
+
+
+
+
     board.update();
     //let Bounding_Box = []
     }
-  setInterval(Calc_and_draw,100);
+  setInterval(Calc_and_draw,50);
 }
+
 function drawmassspringdamper()
 {
   let MSD = new mass_spring_damper(1, 0.1, 5);
   let t = MSD.gettimeseries(0, 100, 10/100);
   let y = MSD.solvemsd(t, [1, 0]);
   //let Bounding_Box = []
-  let board = JXG.JSXGraph.initBoard('app',{pan: {enabled:false},showNavigation:false, browserPan: {enabled:false},axis: true, grid: false});
-  let g3 = board.create('curve', [t.toArray(),y], {strokeColor:'red', strokeWidth:'2px'});
+  let board2 = JXG.JSXGraph.initBoard('app2',{ boundingbox: [0, 1, 100, -1], pan: {enabled:false},showNavigation:false, browserPan: {enabled:false},axis: true, grid: false});
+  let g2 = board2.create('curve', [t.toArray(),y[0]], {strokeColor:'red', strokeWidth:'2px'});
+  board2.update();
 }
 
-//drawmassspringdamper();
+drawmassspringdamper();
 livemassspringdamper();
 //document.querySelector('#app').innerHTML = drawnfunction;
